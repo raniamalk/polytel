611e5ee55f85cfa09a05c9a7ee9ae5608d604ee6
mariem <mariem.sekkat@gmail.com>
2019-03-06 15:37:27 +0000

    06-03-2019

--- src/AppBundle/Controller/marqueController.php
+++ src/AppBundle/Controller/marqueController.php
@@ -12,8 +12,24 @@ use AppBundle\Entity\marque;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Form\marqueType;
 use Sylius\Component\Product\Model\Product;
+use Sylius\Component\Product\Model\ProductTranslation;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductRepository;
+
+
+use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Events;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Sylius\Component\Resource\Metadata\MetadataInterface;
+use Sylius\Component\Resource\Metadata\RegistryInterface;
+use Sylius\Component\Resource\Model\TranslatableInterface;
+use Sylius\Component\Resource\Model\TranslationInterface;
+use Sylius\Component\Resource\Translation\TranslatableEntityLocaleAssignerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
 
@@ -132,15 +148,47 @@ class marqueController extends Controller
        /* $repository = $this->getDoctrine()->getRepository('AppBundle:marque');
         $marque = $repository->findAll();*/
 
-       $marque_name= $request->query->get('marque_name');
 
-        $marque_name=
-                    array(
-                        'marque_name'=>$marque_name,
-                    );
-                $this->get('session')->set('marque_name', $marque_name);
 
-                return new Response(json_encode($marque_name), 200);
+
+
+
+
+     /*  $marque= array('marque_name'=>$marque_name);
+                $this->get('session')->set('marque', $marque);
+
+                return new Response(json_encode($marque), 200);
+
+
+        $repository = $this->getDoctrine()->getRepository('Sylius\Component\Product\Model\Product');
+        $product = $repository->findBy([
+            'marque' => $marque_name,
+        ]);*/
+        $marque_count = $request->request->get('marque_count');
+
+        for ($i=0; $i<=$marque_count; $i++){
+        $marque_name= $request->request->get('marque_name_sjksjksjkjksh'.$i);
+
+
+       /* var_dump($marque_name,$marque_count);die('stop it');*/
+
+        }
+
+
+     /*   for ($j = 0; $j <= $marque_count; $j++) {
+
+            if ($j==0){
+                $marque_name=  $request->request->get('marque_name_sjksjksjkjksh');
+
+            }
+            else{
+                $marque_name=  $request->request->get('marque_name_sjksjksjkjksh'.$j);
+                var_dump($marque_name);die('stop it');
+
+            }
+
+        }*/
+
 
 
         $repository = $this->getDoctrine()->getRepository('Sylius\Component\Product\Model\Product');
@@ -149,19 +197,140 @@ class marqueController extends Controller
         ]);
 
 
-        $em = $this->getDoctrine()->getManager();
 
-//        $query = $em->getRepository('Sylius\Component\Product\Model\Product')->createQueryBuilder('p')->select('*')
-//           /* ->where('p.marque like  :marque')
-//            ->setParameter('marque', $marque)*/
-//            ->getQuery()->getResult();
 
-            /*var_dump($product); die();*/
+
+        /*var_dump($marque_name);
+        die('yes');*/
+
+
+        /*$em = $this->getDoctrine()->getManager();
+
+        $product = $em->getRepository('Sylius\Component\Product\Model\Product')->createQueryBuilder('p')->select('code')
+            ->where('p.marque like  :marque_name')
+            ->setParameter('marque_name', $marque_name)
+            ->getQuery()->getResult();*/
+
+       /* $repository = $this->getDoctrine()
+            ->getRepository(Product::class);
+
+        $query = $repository->createQueryBuilder('p')
+            ->where('p.marque like  :marque_name')
+            ->setParameter('marque_name', $marque_name)
+            ->getQuery();
+
+        $product = $query->getResult();*/
+
+
 
         return $this->render('@App/productsmarques.html.twig', array('product'=>$product));
 
 
     }
 
+
+
+    public function ProductsByMarquenomAction (Request $request,$nom){
+
+        /* $repository = $this->getDoctrine()->getRepository('Sylius\Component\Product\Model\Product');
+         $product = $repository->findAll();*/
+
+        /* $repository = $this->getDoctrine()->getRepository('AppBundle:marque');
+         $marque = $repository->findAll();*/
+
+
+
+
+
+
+
+        /*  $marque= array('marque_name'=>$marque_name);
+                   $this->get('session')->set('marque', $marque);
+
+                   return new Response(json_encode($marque), 200);
+
+
+           $repository = $this->getDoctrine()->getRepository('Sylius\Component\Product\Model\Product');
+           $product = $repository->findBy([
+               'marque' => $marque_name,
+           ]);*/
+
+
+       /* $marque_count = $request->request->get('marque_count');
+
+        for ($i=0; $i<=$marque_count; $i++){
+            $marque_name= $request->request->get('marque_name_sjksjksjkjksh'.$i);
+
+
+             var_dump($marque_name,$marque_count);die('stop it');
+
+        }*/
+
+
+        /*   for ($j = 0; $j <= $marque_count; $j++) {
+
+               if ($j==0){
+                   $marque_name=  $request->request->get('marque_name_sjksjksjkjksh');
+
+               }
+               else{
+                   $marque_name=  $request->request->get('marque_name_sjksjksjkjksh'.$j);
+                   var_dump($marque_name);die('stop it');
+
+               }
+
+           }*/
+
+
+        /*var_dump($marque_name);
+        die('yes');*/
+
+
+
+
+        /* $repository = $this->getDoctrine()
+             ->getRepository(Product::class);
+
+         $query = $repository->createQueryBuilder('p')
+             ->where('p.marque like  :marque_name')
+             ->setParameter('marque_name', $marque_name)
+             ->getQuery();
+
+         $product = $query->getResult();*/
+
+        /*$repository = $this->getDoctrine()->getRepository('Sylius\Component\Product\Model\Product');
+        $product = $repository->findBy([
+            'marque' => $nom,
+        ]);*/
+
+
+
+        $em = $this->getDoctrine()->getManager();
+
+
+
+        $product = $em->getRepository('Sylius\Component\Product\Model\Product')->createQueryBuilder('p')
+            ->where('p.marque like  :marque_name')
+            ->setParameter('marque_name', $nom)
+            /*->leftJoin('p.id', 'sylius_product_translation')*/
+            ->leftJoin(ProductTranslation::class, 't', 'WITH', 't.id = p.id')
+            ->getQuery()->getResult();
+
+
+
+        /*$product = $em->getRepository('Sylius\Component\Product\Model\ProductTranslation')->createQueryBuilder('t')
+            ->Join(Product::class, 'p')
+            ->where('p.marque like  :marque_name')
+            ->setParameter('marque_name', $nom)
+            ->andwhere('p.id = t.translatable_id')
+            ->getQuery()->getResult();*/
+
+
+
+
+        return $this->render('@App/productsmarques.html.twig', array('product'=>$product));
+
+
+    }
 
 }
